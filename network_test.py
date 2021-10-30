1ab03e111dd05e48b3face3248bd9dac75dd56bb
Andrew Reichel <andrew.reichel@gmail.com>
2021-10-29 23:01:27 -0400

    initial commit

---
+++ network_test.py
@@ -0,0 +1,89 @@
+
+import pandas as pd
+import numpy as np
+import datetime, subprocess, socket
+import speedtest, sys, os, time
+
+class run_test:
+    def run_test(test_count=5):
+        bps_mbps = 1000**2
+        print("\n--- Running WiFi Test ---")
+        servers, run_dict=[], {}
+        device_ip = socket.gethostbyname(socket.gethostname())
+        print(device_ip)
+
+        try:
+            wifi = speedtest.Speedtest()
+            #wifi.get_servers(servers)
+            wifi.get_best_server()
+            
+            for i in range(test_count):
+                time_now = datetime.datetime.today()
+                run_time = f"{time_now.year}{time_now.month}{time_now.day}_{time_now.hour}:{time_now.minute}:{time_now.second}"
+
+                download = wifi.download()/(bps_mbps)
+                upload = wifi.upload()/(bps_mbps)
+                test_results = {"down":round(download,2),
+                                    "up":round(upload,2),
+                                    "device_ip":device_ip}
+                run_dict[run_time] = test_results
+
+                print (f"{run_time}:\t{test_results}")
+
+        except:
+            time_now = datetime.datetime.today()
+            run_time = f"{time_now.year}{time_now.month}{time_now.day}_{time_now.hour}:{time_now.minute}:{time_now.second}"
+            print(f"Test Failure: {run_time}")
+
+            test_results = {"down":np.nan,
+                            "up":np.nan,
+                            "device_ip":"None"}
+            run_dict[run_time] = test_results
+        return run_dict
+
+    def restuls_to_df(run_dict):
+        df = pd.DataFrame.from_dict(run_dict, orient='index')
+        return df
+
+
+def write_to_file(file_path:str, df_out:pd.DataFrame):
+    # try to load existing file
+    try:
+        df = pd.read_csv(file_path, index_col=0)
+        to_concat = [df, df_out]
+        df_out = pd.concat(to_concat)
+    except:    
+        print("Something went wrong")
+
+    # TODO: Add check for file size to avoid creating a massive, singular file
+
+    # write file to file_path
+    print(f"Writing dataframe to {file_path}")
+    df_out.to_csv(file_path)
+
+
+if __name__ == "__main__":
+    test_count = 1*10**3
+    sample_rate = 5*60  # tests run per seconds (max)
+
+    run_count, last_run = 0, datetime.datetime.now()
+    while run_count < test_count:
+        last_run = datetime.datetime.now()
+        results = run_test.run_test()
+        res_df = run_test.restuls_to_df(results)
+
+        print(res_df)
+
+        # write file to csv
+        write_to_file("wifi_test.csv", res_df)
+
+        # TODO: Add check for results std dev, re-run test if above a threshold
+
+        run_count += 1
+        time_now = datetime.datetime.now()
+        time_delta = time_now - last_run
+        while time_delta.total_seconds() < sample_rate:
+            print(f"waiting... {round(time_delta.total_seconds() - sample_rate,2)} sec.")
+            time.sleep(30)
+            time_now = datetime.datetime.now()
+            time_delta = time_now - last_run
